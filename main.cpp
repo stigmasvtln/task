513f189f9e62f30fb951c217121be9aded84c813
stigmasvtln <fff672021@gmail.com>
2022-05-31 15:39:10 +0400

    добавлена функция рекурсивного вычисления n-го числа Фибоначчи, в которой последовательность начинается с (1, 1, ...)

--- main.cpp
+++ main.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
+int fib(int n) {
+    if (n == 1 || n == 2) {
+        return 1;
+    } else {
+        return (fib(n-1) + fib(n-2));
+    }
+}
+
 int main() {
-    cout<<"Hello, World!";
+    cout << "Hello, World!" <<endl;;
+    cout << fib(10);
     
     return 0;
 }
